a896b61b74162774f4ff47d094ed4b2a617d6e2f
EDWIN AU <edwinau@rogers.com>
2017-03-29 14:41:03 -0400

    String defs complete

--- ResearchDoc.rb
+++ ResearchDoc.rb
@@ -28,4 +28,19 @@ Most language follos an If then structure.
 
 Description underneath summary describes what the index does.
 
+String::
+  length:
+  1) returns the character length of string
+
+  strip:
+  1) returns a copy of string with leading and triling whitespace removed
+
+  split:
+  1) split(pattern=nil,[limit]) -> array
+  2) Divides string into substrings based on a delimiter, returning an array of these substrings
+
+  start_with?:
+  1) start_with?([prefixes]+) -> true or false
+  2) Returns true if string starts with one of the prefixes given
+
 =end
